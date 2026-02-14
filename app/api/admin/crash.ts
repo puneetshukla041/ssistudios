e2dbd697c2119566fe7636c6c653564b0df7bc14
Puneet Shukla <puneetshukla041@gmail.com>
2026-02-14 22:00:56 +0530

    Update crash.ts

--- app/api/admin/crash.ts
+++ app/api/admin/crash.ts
@@ -6,7 +6,6 @@ import mongoose from 'mongoose';
 import { SystemState } from '@/models/SystemState'; // Ensure path is correct
 
 
-
 const connectDB = async () => {
   if (mongoose.connections[0].readyState) return;s
 
