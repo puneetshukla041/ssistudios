d9d50cc2b06f61399267645b770e564f4820714f
Puneet Shukla <puneetshukla041@gmail.com>
2026-02-18 16:49:35 +0530

    Update page.tsx

--- app/auto/page.tsx
+++ app/auto/page.tsx
@@ -26,6 +26,7 @@ const NAME_MARGIN_LEFT = 72
 const NAME_MARGIN_TOP = 133     
 const HOSPITAL_MARGIN_LEFT = 72 
 
+
 // --- SECOND NAME CONFIGURATION ---
 const SECOND_NAME_MARGIN_LEFT = 98
 // UPDATED: Increased from 206 to 220 to move it down
