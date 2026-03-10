08c4c7e2b813b6deea780f1f3b0646108561d0f4
Puneet Shukla <226358522+puneetshukla041@users.noreply.github.com>
2026-03-10 16:42:20 +0530

    Update route.ts

--- app/api/bug-report/route.ts
+++ app/api/bug-report/route.ts
@@ -10,8 +10,9 @@ import * as mongoose from 'mongoose';
 // We use 'any' for _id since it could be ObjectId or a string UUID depending on how Member is setup.
 interface EmployeeLeanResult {
     _id: any; 
-    username: string;
     
+    username: string;
+
 
 
 }
